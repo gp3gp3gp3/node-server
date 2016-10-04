34a250305b0a2cc6194c6e5946e7eaada79c7596
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-10-04 19:53:54 -0400

    Add sequelize init

--- index.js
+++ index.js
@@ -2,24 +2,18 @@ const express = require('express')
 const http = require('http')
 const app = express()
 const Sequelize = require('sequelize')
+const router = require('./router')
+const db = require('./models')
 
-const db = new Sequelize('gp3-node-server', '', '', {
-  host: 'localhost',
-  dialect: 'postgres',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-})
-
-db.authenticate()
+db.sequelize.authenticate()
   .then(function(err) {
     console.log('Connection has been established successfully')
   })
   .catch(function(err) {
     console.log('Unable to connect to the database', err)
   })
+
+router(app)
 
 const port = process.env.PORT || 3090
 const server = http.createServer(app)

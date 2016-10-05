8f88ecd9740feda65df6aa15e923d2ec913582c2
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-10-05 17:40:35 -0400

    remove obsolete router file

--- router.js
+++
@@ -1,8 +0,0 @@
-const Authentication = require('./controllers/authentication')
-
-module.exports = function(app) {
-  app.get('/', function(req, res) {
-    res.send({ text: "here" })
-  })
-  app.post('/signup', Authentication.signup)
-}

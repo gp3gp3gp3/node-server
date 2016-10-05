263f3232556f68a41068f600ec6a8c7caf7d4a37
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-10-05 15:20:57 -0400

    Add authentication signup route

--- router.js
+++ router.js
@@ -1,5 +1,8 @@
+const Authentication = require('./controllers/authentication')
+
 module.exports = function(app) {
   app.get('/', function(req, res) {
     res.send({ text: "here" })
   })
+  app.post('/signup', Authentication.signup)
 }

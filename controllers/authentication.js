a0f6ecc01019a94a8e85767bcd3050a0b52c5c99
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-10-05 15:20:30 -0400

    Add working controller, no user model response

---
+++ controllers/authentication.js
@@ -0,0 +1,10 @@
+const models = require('../models')
+
+exports.signup = function(req, res, next) {
+  const email = req.body.email
+  const password = req.body.password
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You must provide email and password'})
+  }
+}

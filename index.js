7fe20db5543540f61fccf82b354c9555184f9df5
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-10-04 20:24:29 -0400

    Add middleware for debuggin/cors

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const http = require('http')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 const Sequelize = require('sequelize')
 const router = require('./router')
 const db = require('./models')
 
+// Check db connection
 db.sequelize.authenticate()
   .then(function(err) {
     console.log('Connection has been established successfully')
@@ -13,8 +17,13 @@ db.sequelize.authenticate()
     console.log('Unable to connect to the database', err)
   })
 
+// Middleware
+app.use(bodyParser.json({ type: '*/*' }))
+app.use(morgan('combined'))
+app.use(cors())
 router(app)
 
+// Listen on port
 const port = process.env.PORT || 3090
 const server = http.createServer(app)
 server.listen(port)

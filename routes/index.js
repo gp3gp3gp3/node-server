97fbc7ba7763a324733b2daff5f9a62e94dfd556
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-10-06 13:53:07 -0400

    Require auth for root path

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,11 @@ const passportService = require('../services/passport')
 const passport = require('passport')
 
 const requireSignIn = passport.authenticate('local', { session: false })
+const requireAuth = passport.authenticate('jwt', { session: false })
 
 
-router.get('/', function(req, res) {
-  res.send({ text: "here" })
+router.get('/', requireAuth, function(req, res) {
+  res.send({ message: "Secret code here ABC123" })
 })
 
 router.post('/signup', Authentication.signup)

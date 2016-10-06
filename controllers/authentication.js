99529ad91b983d883855205714b4496206de68a2
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-10-06 12:23:01 -0400

    Add token creation in authentication controller

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,15 @@
+const jwt = require('jwt-simple')
 const User = require('../models').User
+const config = require('../config')
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime()
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret)
+}
+
+exports.signin = function(req, res, next) {
+  res.send({ token: tokenForUser(req.user) })
+}
 
 exports.signup = function(req, res, next) {
   const email = req.body.email
@@ -24,7 +35,7 @@ exports.signup = function(req, res, next) {
 
       user.save()
       .then(function() {
-        res.json({ user: user })
+        res.json({ token: tokenForUser(user) })
       })
     }
   })

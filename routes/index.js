b35590b924535247ec73a090be69f389634dcd69
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-10-06 13:40:52 -0400

    Add signin route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const Authentication = require('../controllers/authentication')
+const passportService = require('../services/passport')
+const passport = require('passport')
+
+const requireSignIn = passport.authenticate('local', { session: false })
 
 
 router.get('/', function(req, res) {
@@ -8,6 +12,7 @@ router.get('/', function(req, res) {
 })
 
 router.post('/signup', Authentication.signup)
+router.post('/signin', requireSignIn, Authentication.signin)
 
 
 module.exports = router

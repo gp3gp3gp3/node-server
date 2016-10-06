df8404c89ae1aaecf4aa9473dc85a6f8c5fe88ba
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-10-06 14:20:41 -0400

    remove comments

--- services/passport.js
+++ services/passport.js
@@ -8,11 +8,9 @@ const LocalStrategy = require('passport-local')
 const localOptions = { usernameField: 'email' }
 const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
 
-  // find user with email
   User.findOne({ where: { email: email }})
   .then(function(existingUser) {
     if(!existingUser) { return done(null, false) }
-  // run compare password with password argument
     if (existingUser.comparePassword(password)) {
       return done(null, existingUser)
     } else {
